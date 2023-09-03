3ac37379f05c244ea63f924c06d82ad3677f0e46
LujanSolo <lujansolo@proton.me>
2023-09-02 22:51:27 -0700

    added initial folder structure for components. Built aboutMe draft 1

---
+++ src/assets/components/aboutMe/About.jsx
@@ -0,0 +1,10 @@
+import './about.css';
+
+export default function About() {
+  return (
+    <div className="grid-container">
+      <h2>a bit about Jamie...</h2>
+      <p>Full-Stack web developer with a focus on front-end development. Full-stack Certificate earned from University of California at Los Angeles' Coding Bootcamp. Experience in design, development, client-based relations, and team leadership built on decades of work in the Film and Television Industry. A passion for technology with an ability to learn new skills quickly.</p>
+    </div>
+  )
+}
