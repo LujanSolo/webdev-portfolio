3e41cd37251f2bd7ada0ba447b4cc4e77054c063
LujanSolo <lujansolo@proton.me>
2023-09-04 01:37:28 -0700

    added background image gradient to page. Played with navbar and customized art icons for eventual link

--- src/assets/components/navbar/Navbar.jsx
+++ src/assets/components/navbar/Navbar.jsx
@@ -1,17 +1,23 @@
 import './navbar.css';
 
-export default function Navbar () {
+export default function Navbar() {
   return (
-    <div >
-            <nav >
-        <ul className='navGrid'>
-          <li>HOME</li>
-          <li>PROJECTS</li>
-          <li>ABOUT ME</li>
-          <li>LINKS</li>
-        </ul>
-      </nav>
-    </div>
+    <nav className="navbar bg-body-primary">
+      <div className="container-fluid myNav">
+        <a className="navbar-brand" href="#">
+          <img src="src/assets/images/proj-icon.png" alt="Logo" width="70" className="d-inline-block align-text-center" />
+          Projects
+        </a>
+        <a className="navbar-brand" href="#">
+          <img src="src/assets/images/github.png" alt="Logo" width="70" className="d-inline-block align-text-center" />
+          GitHub
+        </a>
+        <a className="navbar-brand" href="#">
+          <img src="src/assets/images/linkedin.png" alt="Logo" width="70" className="d-inline-block align-text-center" />
+          LinkedIn
+        </a>
+      </div>
+    </nav>
 
   )
 }
